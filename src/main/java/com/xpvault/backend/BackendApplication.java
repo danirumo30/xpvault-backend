4361bf4c661e2db785e1d75749a069ed5d9f33b4
Kidou <daniel.rubio39@educa.madrid.org>
2025-05-16 14:53:44 +0200

    Merge branch 'developer-dan'

--- src/main/java/com/xpvault/backend/BackendApplication.java
+++ src/main/java/com/xpvault/backend/BackendApplication.java
@@ -2,10 +2,8 @@ package com.xpvault.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:spring/springmvc-context.xml")
 public class BackendApplication {
 
     public static void main(String[] args) {
