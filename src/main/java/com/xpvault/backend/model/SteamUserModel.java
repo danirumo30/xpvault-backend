b2ba1a81edf6d25a11e30818d02599d70c4c638e
Kidou <daniel.rubio39@educa.madrid.org>
2025-05-22 14:04:16 +0200

    Add movies and series to profile.

--- src/main/java/com/xpvault/backend/model/SteamUserModel.java
+++ src/main/java/com/xpvault/backend/model/SteamUserModel.java
@@ -13,6 +13,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Entity
 @Table(name = "steam_user")
 @AllArgsConstructor
@@ -20,7 +23,10 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
-public class SteamUserModel {
+public class SteamUserModel implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
